4bcd23f3be721e2466413349ea588f76f06b3fc7
SukkaW <isukkaw@gmail.com>
2018-08-21 18:55:51 +0800

    feat(omz): add install script

---
+++ omz.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install zsh
+sudo apt install curl
+sudo apt install fonts-powerline
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
