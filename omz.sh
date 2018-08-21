aac86714363e6e65c8ab8f9bac6c1f0f6074e20a
SukkaW <isukkaw@gmail.com>
2018-08-21 20:15:27 +0800

    feat(omz): add alias

--- omz.sh
+++ omz.sh
@@ -6,3 +6,5 @@ sudo apt install fonts-powerline
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+
+cat alias4zshrc >> ~/.zshrc
