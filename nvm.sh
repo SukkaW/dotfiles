cd2566a7d700a962a19ab17f808b4471fc651064
SukkaW <isukkaw@gmail.com>
2018-08-21 20:18:33 +0800

    feat(nvm): add install script

---
+++ nvm.sh
@@ -0,0 +1,11 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+cat nvm4zshrc >> ~/.zshrc
+
+source ~/.zshrc
+
+command -v nvm
+
+nvm install node
+
+nvm use node
