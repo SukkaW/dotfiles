d458253b7b9f5f45048e8852e0f0846a43916b3c
SukkaW <isukkaw@gmail.com>
2018-11-16 23:59:54 +0800

    feat(node): finish

---
+++ _install/yarn.sh
@@ -0,0 +1,9 @@
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install --no-install-recommends yarn
+
+echo "-----------------------------------------------------------"
+echo "Output Yarn Version"
+echo "-----------------------------------------------------------"
+
+yarn --version
