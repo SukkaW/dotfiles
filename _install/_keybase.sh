b58dc581c8ab9f384583c55810b8a48bd5b819ce
Sukka <isukkaw@gmail.com>
2018-11-24 00:59:52 +0800

    feat(keybase): add install scripts

---
+++ _install/_keybase.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+curl -O https://prerelease.keybase.io/keybase_amd64.deb
+# if you see an error about missing `libappindicator1`
+# from the next command, you can ignore it, as the
+# subsequent command corrects it
+sudo dpkg -i keybase_amd64.deb
+sudo apt-get install -f
