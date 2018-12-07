77d790ac00ec731b1ed34bb75cc8e42b43bbf81e
SukkaW <isukkaw@gmail.com>
2018-12-07 20:58:13 +0800

    update(omz): use ys zsh theme

--- _install/_omz.sh
+++ _install/_omz.sh
@@ -2,5 +2,5 @@
 
 sh -c "$(curl -fsSL http://cdn.jsdelivr.net/gh/robbyrussell/oh-my-zsh@master/tools/install.sh)"
 
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' $HOME/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' $HOME/.zshrc
 
