d56b600a06195877522401df3280d2b3c6fcbe2f
Sukka <isukkaw@gmail.com>
2018-12-09 14:41:48 +0800

    feat(cs50): bring up set mirror shell

---
+++ cs50ide-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo sed -i 's|http://ppa.launchpad.net|http://launchpad.speedtests.ml|g' /etc/apt/sources.list.d/*.list                                                                                                            
+sudo sed -i 's|http://us-central1.gce.clouds.archive.ubuntu.com|http://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
+sudo sed -i 's|http://security.ubuntu.com|http://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
