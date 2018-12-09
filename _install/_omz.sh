83da9bd732990cbb93db7b297c75946dc2cbf500
SukkaW <isukkaw@gmail.com>
2018-12-09 23:37:47 +0800

    feat(env-setup): enable zsh plugin

--- _install/_omz.sh
+++ _install/_omz.sh
@@ -4,7 +4,7 @@ sh -c "$(curl -fsSL http://cdn.jsdelivr.net/gh/robbyrussell/oh-my-zsh@master/too
 
 echo -n "
 -----------------------------------------------------------
-Install sukka.zsh-theme ...
+Install sukka.zsh-theme and custom pkugins ...
 ----------------------------------------------------------
 "
 
@@ -15,4 +15,8 @@ cp -r ./zsh-plugins/. $HOME/.oh-my-zsh/custom/plugins/
 
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="sukka"/g' $HOME/.zshrc
 
+sed -i 's|plugins=(|plugins=(\n  proxy|g' $HOME/.zshrc
+
 tree $HOME/.oh-my-zsh/custom/
+
+cat $HOME/.zshrc
