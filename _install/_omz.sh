42e28f9f229bd7a57e9e2e994eee33c62a73c821
SukkaW <isukkaw@gmail.com>
2018-12-09 22:45:48 +0800

    feat: update custom zsh import

--- _install/_omz.sh
+++ _install/_omz.sh
@@ -10,6 +10,9 @@ Install sukka.zsh-theme ...
 
 touch $HOME/.oh-my-zsh/themes/sukka.zsh-theme
 
-cat ./zsh-theme/sukka.zsh-theme > $HOME/.oh-my-zsh/themes/sukka.zsh-theme
+cp ./zsh-theme/. $HOME/.oh-my-zsh/custom/themes/
+cp ./zsh-theme/. $HOME/.oh-my-zsh/custom/plugins/
 
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="sukka"/g' $HOME/.zshrc
+
+tree $HOME/.oh-my-zsh/custom/
