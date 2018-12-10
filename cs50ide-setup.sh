8137494ffbb1fd9272af0094028f87968ffd644e
Sukka <isukkaw@gmail.com>
2018-12-10 22:38:52 +0800

    feat(cs50): add new mirror replacement

--- cs50ide-setup.sh
+++ cs50ide-setup.sh
@@ -2,4 +2,5 @@
 
 sudo sed -i 's|http://ppa.launchpad.net|http://launchpad.speedtests.ml|g' /etc/apt/sources.list.d/*.list
 sudo sed -i 's|http://us-central1.gce.clouds.archive.ubuntu.com|http://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
+sudo sed -i 's|http://asia-east1.gce.clouds.archive.ubuntu.com|https://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
 sudo sed -i 's|http://security.ubuntu.com|http://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
