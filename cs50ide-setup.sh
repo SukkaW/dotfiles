1462192274a31bae1f6aced2fac1c7b83a9e0bb7
Sukka <isukkaw@gmail.com>
2018-12-13 22:15:02 +0800

    feat(cs50): add basic package

--- cs50ide-setup.sh
+++ cs50ide-setup.sh
@@ -6,3 +6,6 @@ sudo sed -i 's|http://security.ubuntu.com|http://mirrors.speedtests.ml|g' /etc/a
 sudo sed -i 's|http://us-central1.gce.clouds.archive.ubuntu.com|http://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
 sudo sed -i 's|http://asia-east1.gce.clouds.archive.ubuntu.com|http://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
 sudo sed -i 's|http://europe-west1.gce.clouds.archive.ubuntu.com|http://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
+
+sudo apt update
+sudo apt install -y curl git tree python2.7 python3-dev python3-pip python3-setuptools whois axel
