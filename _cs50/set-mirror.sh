9882b6d46879b3415cb30d2a69bb1ab1f83b83b2
SukkaW <isukkaw@gmail.com>
2018-12-15 00:15:00 +0800

    feat(cs50): use self host mongodb mirror

--- _cs50/set-mirror.sh
+++ _cs50/set-mirror.sh
@@ -2,6 +2,7 @@
 
 sudo sed -i 's|http://ppa.launchpad.net|https://launchpad.speedtests.ml|g' /etc/apt/sources.list.d/*.list
 sudo sed -i 's|http://security.ubuntu.com|https://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
+sudo sed -i 's|https://downloads-distro.mongodb.org|https://mongodb.speedtests.ml|g' /etc/apt/sources.list.d/*.list
 
 sudo sed -i 's|http://us-central1.gce.clouds.archive.ubuntu.com|https://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
 sudo sed -i 's|http://asia-east1.gce.clouds.archive.ubuntu.com|https://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
