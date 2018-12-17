98c746e0bf60e50c050100fae32d4924802eaff4
Sukka <isukkaw@gmail.com>
2018-12-17 18:11:30 +0800

    refactor(cs50): change ubuntu mirror

--- _cs50/set-mirror.sh
+++ _cs50/set-mirror.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo sed -i 's|http://ppa.launchpad.net|https://launchpad.speedtests.ml|g' /etc/apt/sources.list.d/*.list
-sudo sed -i 's|http://security.ubuntu.com|https://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
+sudo sed -i 's|http://security.ubuntu.com|https://ubuntu.speedtests.ml|g' /etc/apt/sources.list.d/*.list
 sudo sed -i 's|https://downloads-distro.mongodb.org|https://mongodb.speedtests.ml|g' /etc/apt/sources.list.d/*.list
 
-sudo sed -i 's|http://us-central1.gce.clouds.archive.ubuntu.com|https://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
-sudo sed -i 's|http://asia-east1.gce.clouds.archive.ubuntu.com|https://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
-sudo sed -i 's|http://europe-west1.gce.clouds.archive.ubuntu.com|https://mirrors.speedtests.ml|g' /etc/apt/sources.list.d/*.list
+sudo sed -i 's|http://us-central1.gce.clouds.archive.ubuntu.com|https://ubuntu.speedtests.ml|g' /etc/apt/sources.list.d/*.list
+sudo sed -i 's|http://asia-east1.gce.clouds.archive.ubuntu.com|https://ubuntu.speedtests.ml|g' /etc/apt/sources.list.d/*.list
+sudo sed -i 's|http://europe-west1.gce.clouds.archive.ubuntu.com|https://ubuntu.speedtests.ml|g' /etc/apt/sources.list.d/*.list
