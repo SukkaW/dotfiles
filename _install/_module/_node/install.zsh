573b80193a30b14fb64e58fb1c5eb3af58cb7afb
SukkaW <isukkaw@gmail.com>
2018-12-19 14:15:36 +0800

    refactor(node): split into 3 parts

--- _install/_module/_node/install.zsh
+++ _install/_module/_node/install.zsh
@@ -4,66 +4,11 @@ echo -n "
 ============================================================
               Setting up NodeJS Environment
 ------------------------------------------------------------
-* Installing NVM...
-------------------------------------------------------------
 "
 
-curl -o- https://cdn.jsdelivr.net/gh/creationix/nvm@v0.33.11/install.sh | bash
-
-source $HOME/.zshrc
-
-echo -n "
-------------------------------------------------------------
-* Import nvm script to .zshrc... "
-
-_NVM_RC=$(cat $HOME/.zshrc | grep 'export NVM_DIR')
-
-if [ ! -n "${_NVM_RC}" ]; then
-    cat ./_install/_node/nvm.zshrc | tee $HOME/.zshrc
-fi
-
-echo -n "Done!
-------------------------------------------------------------
-"
-
-source $HOME/.zshrc
-
-echo -n "
-------------------------------------------------------------
-* NVM Verision: "
-
-command -v nvm
-
-echo -n "
-------------------------------------------------------------
-* Installing NodeJS 10.8.0...
-------------------------------------------------------------
-"
-
-nvm install v10.8.0
-
-echo -n "
-------------------------------------------------------------
-* Set NodeJS 10.8.0 as default... "
-
-nvm use v10.8.0
-nvm alias default v10.8.0
-
-echo -n "Done!
------------------------------------------------------------
-* Installing Yarn...
------------------------------------------------------------
-"
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install --no-install-recommends yarn
-
-echo -n "
------------------------------------------------------------
-* Yarn Version: "
-
-yarn --version
+./_install/_module/_node/_nvm.zsh
+./_install/_module/_node/_node.sh
+./_install/_module/_node/_yarn.sh
 
 echo -n "-----------------------------------------------------------
 * Yarn Global Add those packages:
