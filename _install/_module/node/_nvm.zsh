e0e34b31d62d0657f0f5b9a0aa00198a8b9dd700
SukkaW <isukkaw@gmail.com>
2018-12-31 03:37:53 +0800

    fix(node): fix nvm version output

--- _install/_module/node/_nvm.zsh
+++ _install/_module/node/_nvm.zsh
@@ -7,7 +7,9 @@ echo -n "
 
 curl -o- https://cdn.jsdelivr.net/gh/creationix/nvm@v0.33.11/install.sh | bash
 
-source $HOME/.zshrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo -n "
 ------------------------------------------------------------
