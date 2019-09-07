b1dff9373b4b8f3381fe32c95fc1cfa5ae8e4719
Sukka <isukkaw@gmail.com>
2019-09-07 21:03:22 +0800

    feat(zsh): add zsh-ipip & zsh-nvm to install.sh

--- _install/wlinux.sh
+++ _install/wlinux.sh
@@ -89,6 +89,8 @@ setup-omz() {
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/sukkaw/zsh-proxy.git ~/.oh-my-zsh/custom/plugins/zsh-proxy
+    git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+    git clone https://github.com/sukkaw/zsh-ipip.git ~/.oh-my-zsh/custom/plugins/zsh-ipip
 
     cp -r ./zsh-theme/. $HOME/.oh-my-zsh/custom/themes/
     cp -r ./zsh-plugins/. $HOME/.oh-my-zsh/custom/plugins/
